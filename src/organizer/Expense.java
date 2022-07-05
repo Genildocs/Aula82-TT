70a17201c16a48a37c68a06ca4e53c07cfcf1fe7
Genildo <genildocs@gmail.com>
2022-07-05 03:28:01 -0300

    Segundo comit

--- src/organizer/Expense.java
+++ src/organizer/Expense.java
@@ -1,2 +1,62 @@
-package expenses;public class Expense {
+package organizer;
+
+public class Expense {
+    // private não consigo acessar fora da classe apenas com metodos.
+    //Declarando os atributos da classe.
+    private int id;
+    private String name;
+    private double amount;
+    private String expireDate;
+    private String category;
+    //Declarando o metodo construtor
+    public Expense(int id, String name,double amount, String expireDate, String category){
+
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.expireDate = expireDate;
+        this.category = category;
+    }
+    // Declarando os metodos get e set.
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(){
+        this.expireDate = expireDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
 }
--- src/organizer/ExpenseStream.java
+++ src/organizer/ExpenseStream.java
@@ -1,2 +1,39 @@
-package organizer;public class ExpenseStream {
+package organizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Scanner;
+import java.util.Comparator;
+
+public class ExpenseStream {
+
+    public static void main(String[] args) {
+
+        List<Expense> expenseList = new ArrayList<Expense>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe a quantidade de boletos: ");
+        int qt = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 0; i < qt; i++){
+
+            int id = i;
+
+            System.out.print("Informe o nome do boleto ["+ i + "]: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Informe o valor do boleto R$ ["+ i + "]: ");
+            double amount = Double.parseDouble(scanner.nextLine());
+
+            System.out.print("Informe a data de vencimento do boleto ["+ i + "]: ");
+            String expireDate = scanner.nextLine();
+
+            System.out.print("Informe a categoria do boleto ["+ i + "]: ");
+            String category = scanner.nextLine();
+
+            expenseList.add(new Expense(id, name, amount, expireDate, category));
+        }
+    }
 }
